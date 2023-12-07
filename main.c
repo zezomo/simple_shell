f0e9cd87dd939885095e224fe8aab9c6e926e49a
zezomo <zezouniverse7@gmail.com>
2023-12-07 08:00:01 -0800

    zezo

--- main.c
+++ main.c
@@ -5,22 +5,41 @@
 #include <sys/wait.h>
 
 #define MAX_COMMAND_LENGTH 100
+/**
+ * display_prompt - this is a function believe me
+ *
+ * Return: nothing just print
+ */
 
-void display_prompt() {
-        printf("#cisfun$ ");
+void display_prompt(void)
+{
+	char prompt[] = "#cisfun$ ";
+
+	write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
 }
 
-int main()
+/**
+ * main - it is a main fun
+ *
+ * Return: dw dwdmwo dmqod dq dq d
+ */
+
+int main(void)
 {
-	char input[MAX_COMMAND_LENGTH];
+	char *input = NULL;
+	size_t len = 0;
+	ssize_t read;
 	pid_t pid;
 
-	while (1) {
+	while (1)
+	{
 		display_prompt();
 
-		if (fgets(input, MAX_COMMAND_LENGTH, stdin) == NULL)
+		read = getline(&input, &len, stdin);
+
+		if (read == -1)
 		{
-			printf("\n");
+			write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
 
@@ -43,6 +62,8 @@ int main()
 			waitpid(pid, NULL, 0);
 		}
 	}
+	free(input);
 
 	return (0);
 }
+
